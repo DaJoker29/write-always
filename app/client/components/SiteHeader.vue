39cbdc57c37b592553157b9b7d3e1bcbe2b002b1
Dewitt Buckingham <dj729@comcast.net>
2019-01-18 00:03:07 -0800

    fix: change color on progress-bar peg

--- app/client/components/SiteHeader.vue
+++ app/client/components/SiteHeader.vue
@@ -215,7 +215,7 @@ export default {
   right: 0px;
   width: 100px;
   height: 100%;
-  box-shadow: 0 0 10px #29d, 0 0 5px #29d;
+  box-shadow: 0 0 10px var(--color-black), 0 0 5px var(--color-black);
   opacity: 1;
 
   -webkit-transform: rotate(3deg) translate(0px, -4px);
