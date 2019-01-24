ce8af1d1f792a298f9180090b1e883213b872c8c
Dewitt Buckingham <dj729@comcast.net>
2019-01-24 12:54:35 -0800

    feat: add sidebar

---
+++ app/client/components/TheSidebar.vue
@@ -0,0 +1,23 @@
+<template>
+  <aside>
+    <h3>Welcome back, {{ user.displayName }}!</h3>
+  </aside>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+aside {
+  background-color: var(--color-black);
+  color: var(--color-white);
+}
+</style>
--- app/client/views/Home.vue
+++ app/client/views/Home.vue
@@ -10,34 +10,70 @@
         <a href=""> please contact one of our admins. </a>
       </p>
     </aside>
-    <main><NotebookList :notebooks="notebooks" /></main>
+    <main>
+      <TheSidebar
+        v-if="isLoggedIn && Object.entries(user).length > 0"
+        :user="user"
+      />
+      <NotebookList :notebooks="notebooks" />
+    </main>
   </div>
 </template>
 
 <script>
 import NotebookList from '@client/components/NotebookList';
+import TheSidebar from '@client/components/TheSidebar';
+import { mapGetters, mapState } from 'vuex';
 
 export default {
   components: {
-    NotebookList
+    NotebookList,
+    TheSidebar
   },
   data: function() {
     return {
-      notebooks: []
+      notebooks: [],
+      user: {}
     };
+  },
+  computed: {
+    ...mapGetters(['isLoggedIn']),
+    ...mapState(['uid'])
   },
   created: async function() {
     this.notebooks = await this.fetchNotebooks();
+    this.user = await this.fetchUser();
   },
   methods: {
     fetchNotebooks: async function() {
       return (await this.$http.get('/notebooks')).data;
+    },
+    fetchUser: async function() {
+      return (await this.$http.get(`/user/${this.uid}`)).data;
     }
   }
 };
 </script>
 
 <style scoped>
+main {
+  display: flex;
+  flex-flow: row wrap;
+  margin: 0;
+}
+
+main > /deep/ * {
+  padding: var(--spacing);
+}
+
+main > /deep/ aside {
+  flex: 1;
+}
+
+main > /deep/ section {
+  flex: 3;
+}
+
 .intro {
   padding: var(--spacing-half) var(--spacing);
   background-color: var(--color-red);
