b1fddba66ed75ba30ff98961a2e54034270da5c7
Dewitt Buckingham <dj729@comcast.net>
2019-01-30 15:53:57 -0800

    style: style single author page

--- app/client/components/SingleAuthorHeader.vue
+++ app/client/components/SingleAuthorHeader.vue
@@ -1,7 +1,8 @@
 <template>
   <section>
     <h2>{{ author.displayName }}</h2>
-    <p>{{ author.location }}</p>
+    <p v-if="active()" class="active">Currently active</p>
+    <p v-if="author.location">From {{ author.location }}</p>
   </section>
 </template>
 
@@ -12,6 +13,20 @@ export default {
       type: Object,
       required: true
     }
+  },
+  methods: {
+    active() {
+      const now = this.moment();
+      const lastLogin = this.moment(this.author.dateLastLogin);
+      return now.diff(lastLogin, 'minutes') < 15;
+    }
   }
 };
 </script>
+
+<style scoped>
+.active {
+  color: var(--color-red);
+  font-weight: bold;
+}
+</style>
