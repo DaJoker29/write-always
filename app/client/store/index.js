cbbfc8a9856f6d964e22e9950073a9a519013afb
Dewitt Buckingham <dj729@comcast.net>
2019-02-13 17:32:15 -0800

    refactor: add auth module to store
    Closes #40

--- app/client/store/index.js
+++ app/client/store/index.js
@@ -1,55 +1,33 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import http from '@client/http-common';
-import axios from 'axios';
 import todos from './todos';
 import ui from './ui';
+import auth from './auth';
 import sort, { sortEntries, sortNotebooks, sortUsers } from './sort';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
+    auth,
     ui,
     sort,
     todos
   },
   state: {
     config: process.env.SITE_CONFIG,
-    token: localStorage.getItem('token') || '',
-    currentUser: JSON.parse(localStorage.getItem('currentUser')) || {},
     allUsers: JSON.parse(localStorage.getItem('allUsers')) || [],
     allNotebooks: JSON.parse(localStorage.getItem('allNotebooks')) || [],
     allEntries: JSON.parse(localStorage.getItem('allEntries')) || []
   },
   getters: {
     config: state => state.config,
-    isLoggedIn: state =>
-      Object.keys(state.currentUser).length !== 0 &&
-      state.currentUser.constructor === Object,
-
-    currentUser: state => state.currentUser,
     allNotebooks: state => sortNotebooks(state),
     allUsers: state => sortUsers(state),
     allEntries: state => sortEntries(state)
   },
   mutations: {
-    setToken(state, payload) {
-      state.token = payload;
-      localStorage.setItem('token', payload);
-    },
-    logout(state) {
-      state.token = '';
-      state.currentUser = {};
-      state.todos.todos = [];
-      localStorage.removeItem('token');
-      localStorage.removeItem('currentUser');
-      localStorage.removeItem('todos');
-    },
-    updateCurrentUser(state, payload) {
-      state.currentUser = payload;
-      localStorage.setItem('currentUser', JSON.stringify(payload));
-    },
     updateAllUsers(state, payload) {
       state.allUsers = payload;
       localStorage.setItem('allUsers', JSON.stringify(payload));
@@ -68,63 +46,6 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    fbLogin({ dispatch }, { email, ...response }) {
-      dispatch('fetchToken', { method: 'fb', email, response });
-    },
-    fbLogout() {
-      window.FB.logout();
-    },
-    logout({ commit, dispatch }) {
-      commit('logout');
-      dispatch('initialFetch');
-    },
-    async updateFBToken({ dispatch }, payload) {
-      await http.post('/user/fb', payload);
-      dispatch('fetchUser');
-    },
-    loginToFacebook({ dispatch }) {
-      window.FB.login(
-        ({ authResponse }) => {
-          if (authResponse) {
-            window.FB.api('/me', { fields: 'name, email' }, response => {
-              const { name: displayName, id: fbUserID, email } = response;
-              const { accessToken: fbUserAccess } = authResponse;
-
-              const payload = {
-                email,
-                displayName,
-                fbUserID,
-                fbUserAccess
-              };
-
-              dispatch('fbLogin', payload);
-            });
-          } else {
-            console.log('User cancelled login or did not fully authorize');
-          }
-        },
-        { scope: 'email' }
-      );
-    },
-    checkFBStatus({ dispatch, state }) {
-      window.FB.getLoginStatus(({ status, authResponse }) => {
-        if (authResponse && status === 'connected' && state.token) {
-          // User authenticated through Facebook and ready to roll
-          const { userID: fbUserID, accessToken: fbUserAccess } = authResponse;
-          dispatch('updateFBToken', { fbUserID, fbUserAccess });
-        } else if (
-          status === 'authorization_expired' ||
-          status === 'not_authorized'
-        ) {
-          // Problem with facebook authorization. :'(
-          dispatch('fbLogout');
-          dispatch('logout');
-        } else {
-          // What's facebook?
-          dispatch('logout');
-        }
-      });
-    },
     initialFetch({ dispatch }) {
       dispatch('fetchAllNotebooks');
     },
@@ -151,17 +72,6 @@ export default new Vuex.Store({
         (await http.post('/entries/recent', {
           notebooks: state.allNotebooks.map(e => e._id)
         })).data
-      );
-    },
-    async fetchToken({ commit, dispatch }, payload) {
-      commit('setToken', (await axios.post('/auth/login', payload)).data);
-      dispatch('fetchUser');
-      dispatch('fetchAllNotebooks');
-    },
-    async fetchUser({ commit, state }) {
-      commit(
-        'updateCurrentUser',
-        (await http.post('/user/token', { token: state.token })).data
       );
     },
     async fetchAllUsers({ commit }) {
