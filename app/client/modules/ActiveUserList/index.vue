62f60442ac1fb0d373c31b99a75e54b0ded99547
Dewitt Buckingham <dj729@comcast.net>
2019-03-06 23:49:34 -0800

    chore: mockup active user list

--- app/client/modules/ActiveUserList/index.vue
+++ app/client/modules/ActiveUserList/index.vue
@@ -1,3 +1,46 @@
-<template
-  ><div>Active User List Goes Here...</div></template
->
+<template>
+  <aside>
+    <small>3 Users Currently Active</small>
+    <ul>
+      <li class="active">User #1</li>
+      <li class="active">User #2</li>
+      <li>User #3</li>
+      <li class="active">User #4</li>
+      <li>User #5</li>
+    </ul>
+  </aside>
+</template>
+
+<style scoped>
+aside {
+  padding: var(--spacing);
+}
+
+ul {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-end;
+  margin: var(--spacing-half) 0;
+}
+
+ul > li {
+  margin: 0 var(--spacing-half);
+  padding: 0 var(--spacing-half);
+  font-size: var(--small);
+  display: inline;
+  border-radius: var(--spacing-half);
+  background-color: var(--color-grey);
+  color: var(--color-white);
+}
+
+.active {
+  background-color: var(--color-blue);
+}
+
+small {
+  display: block;
+  color: var(--color-grey);
+  font-style: italic;
+  text-align: right;
+}
+</style>
